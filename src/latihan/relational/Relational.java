92a5acbbd2d939450f37dfaa193432f6d54d2cf9
Wildhevire <wildhevire404@gmail.com>
2019-09-26 18:11:07 +0000

    finished

---
+++ src/latihan/relational/Relational.java
@@ -0,0 +1,20 @@
+package latihan.relational;
+
+public class Relational {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : Implementasi operasi relational
+     */
+	 public static void main(String[] args) {
+	        int bil1 = 10;
+	        int bil2 = 20;
+	        System.out.println("a == b = " + (bil1 == bil2));
+	        System.out.println("a != b = " + (bil1 != bil2));
+	        System.out.println("a > b = " + (bil1 > bil2));
+	        System.out.println("a < b = " + (bil1 < bil2));
+	        System.out.println("b >= a = " + (bil2 >= bil1));
+	        System.out.println("b <= a = " + (bil2 <= bil1));
+	    }
+}
